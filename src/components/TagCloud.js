dcc7e0972c78ec59fb1412f23f341455c1352bf7
fuboat <fuboat@outlook.com>
2022-12-25 02:01:54 +0800

    tagpie variable update

--- src/components/TagCloud.js
+++ src/components/TagCloud.js
@@ -1,7 +1,7 @@
 import { WordCloud } from '@ant-design/plots'
 // import * as d3 from 'd3-scale-chromatic'
 import {scheme} from '../palette'
-import {clickedTag} from './TagPieSelector'
+import {onClickedTag} from './TagPieSelector'
 
 export default function ({data, style}) {
   data = Object.keys(data).map(tag => ({tag, count: data[tag]}))
@@ -36,7 +36,9 @@ export default function ({data, style}) {
       plot.on('element:click', e => {
         const datum = e?.data?.data?.datum
         console.log('e', datum)
-        clickedTag(datum.tag)
+        if (onClickedTag) {
+          onClickedTag(datum.tag)
+        }
       })
     }
   }
--- src/components/TagPieSelector.js
+++ src/components/TagPieSelector.js
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 import TagPie from '../components/TagPie'
 
-export var clickedTag = 'HIP HOP'
-export function setClickedTag(tagName) {
-  clickedTag = tagName
+export var onClickedTag = undefined
+export function setClickedTagCallback(callbackFunc) {
+  onClickedTag = callbackFunc
 }
 
 export default function ({data, style}) {
 
   const [state, setState] = useState('HIP HOP')
-  setClickedTag((tagName) => { console.log("tagview: tag is clicked"); setState(tagName) })
+  setClickedTagCallback((tagName) => { console.log("tagview: tag is clicked"); setState(tagName) })
   
   console.log("tagpie", data);
 
